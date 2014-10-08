5b3897777fc980680439fa77f07bf404e634917d
Jonas McCallum <jonasmccallum@gmail.com>
2014-10-08 22:39:20 +1100

    add python 2.7 support

--- README.md
+++ README.md
@@ -3,9 +3,6 @@ autocorrect
 
 Python 3 Spelling Corrector
 
-    Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 00:54:21) 
-    [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-    Type "help", "copyright", "credits" or "license" for more information.
     >>> from autocorrect.spell import spell
     >>> spell('TGhe')
     'The'
--- autocorrect/utils.py
+++ autocorrect/utils.py
@@ -14,6 +14,7 @@ https://github.com/foobarmus/autocorrect
 
 """
 import re, os, tarfile
+from contextlib import closing
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 SOURCE_DIR = os.path.split(PATH)[0]
@@ -24,9 +25,9 @@ def words_from_archive(filename, map_case=False):
     """extract words from a text file in the archive"""
     bz2 = os.path.join(SOURCE_DIR, BZ2)
     tar_path = os.path.join('words', filename)
-    with tarfile.open(bz2, 'r:bz2') as t:
-        with t.extractfile(tar_path) as f:
-            words = set(re.findall(RE, f.read().decode()))
+    with closing(tarfile.open(bz2, 'r:bz2')) as t:
+        with closing(t.extractfile(tar_path)) as f:
+            words = set(re.findall(RE, f.read().decode(encoding='utf-8')))
     if map_case:
         return {w.lower():w for w in words}
     return words
