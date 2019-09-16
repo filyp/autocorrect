b87998a7f9ee9e9003772fe6bb86f67436dd2cd1
fifimajster <filipsondej@protonmail.com>
2019-09-16 03:12:49 +0200

    new release

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 
 setup(name='autocorrect',
-      version='0.4.0',
+      version='0.4.1',
       packages=['autocorrect'],
+      package_data={'autocorrect': ['../data/*.tar.gz']},
       description='Python 3 Spelling Corrector',
       author='Jonas McCallum',
       author_email='jonasmccallum@gmail.com',
