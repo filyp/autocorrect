f352dc6076fa784724f395f57cbe5aa74146446a
fifimajster <filipsondej@protonmail.com>
2019-09-17 02:07:10 +0200

    new release reference old repo to keep github stats

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='autocorrect',
-      version='0.4.3',
+      version='0.4.4',
       packages=['autocorrect'],
       package_data={'autocorrect': ['data/*.tar.gz']},
       description='Python 3 Spelling Corrector',
       author='Jonas McCallum',
       author_email='jonasmccallum@gmail.com',
-      url='https://github.com/fifimajster/autocorrect',
+      url='https://github.com/phatpiglet/autocorrect/',
       license='http://www.opensource.org/licenses/mit-license.php',
       classifiers=('Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT License',
