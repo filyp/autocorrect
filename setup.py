92b54f9a8e2d9c7f0ee329e422c1257e048a9ddf
fsondej <filipsondej@protonmail.com>
2020-03-25 18:55:44 +0100

    new release

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='autocorrect',
-      version='0.4.4',
+      version='1.0.0',
       packages=['autocorrect'],
       package_data={'autocorrect': ['data/*.tar.gz']},
-      description='Python 3 Spelling Corrector',
-      author='Jonas McCallum',
-      author_email='jonasmccallum@gmail.com',
-      url='https://github.com/phatpiglet/autocorrect/',
+      description='Spelling Corrector',
+      author='Jonas McCallum, Filip Sondej',
+      author_email='filipsondej@protonmail.com',
+      url='https://github.com/fsondej/autocorrect',
       license='http://www.opensource.org/licenses/mit-license.php',
       classifiers=('Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT License',
