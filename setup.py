25f42bbaf1deacc5c9618b86fcff579983facab5
fsondej <filipsondej@protonmail.com>
2020-09-12 13:38:15 +0200

    fix setup.py to use python3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='autocorrect',
       version='2.0.0',
-      python_requires='>3.6',
+      python_requires='>=3.6',
       packages=['autocorrect'],
       package_data={'autocorrect': ['data/en.tar.gz']},
       description='Spelling Corrector',
