debfdf6d63c7b794d1719acdb5da59af98a95ad1
Martin Vejvar <vejvarm@gmail.com>
2020-09-12 21:58:45 +0200

    feat: add support for Czech language and swap dl links to working ones

--- autocorrect/constants.py
+++ autocorrect/constants.py
@@ -5,6 +5,7 @@ word_regexes = {
     'uk': r'[АаБбВвГгҐґДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЬЮюЯя]+',
     'tr': r'[a-zA-ZçÇğĞüÜöÖşŞıİ]+',
     'es': r'[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+',
+    'cs': r'[AÁBCČDĎEÉĚFGH(Ch)IÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽaábcčdďeéěfgh(ch)iíjklmnňoópqrřsštťuúůvwxyýzž]+',
 }
 
 alphabets = {
@@ -14,13 +15,15 @@ alphabets = {
     'uk': 'фагксщроємшплуьцнжхїйювязтибґідеч',
     'tr': 'abcçdefgğhıijklmnoöprsştuüvyzqwxÇĞİÜÖ',
     'es': 'abcdefghijklmnopqrstuvwxyzáéíóúüñ',
+    'cs': 'aábcčdďeéěfgh(ch)iíjklmnňoópqrřsštťuúůvwxyýzž.',
 }
 
 urls = {
-    'en': 'https://drive.google.com/uc?export=download&id=19xqFyk9d8mFR7LR43oy6cExk8Pk9wVwV',
-    'pl': 'https://drive.google.com/uc?export=download&id=1kqGWZLUsE8YAB-_nvfMJXNTjYBsjIvSN',
-    'ru': 'https://drive.google.com/uc?export=download&id=1vvOako2MwtexEHX24Jx5CjjN-1yEXOrf',
-    'uk': 'https://drive.google.com/uc?export=download&id=1nG_DG6aWU1PUA_DPSfrvh92puTLr8-aA',
-    'tr': 'https://drive.google.com/uc?export=download&id=1ZGQEhLTteTQprRpnKrHTFWeV2RxYny21',
-    'es': 'https://drive.google.com/uc?export=download&id=19VQuOE12sb0p10SBKWRbSdnUvk0eeFPg',
+    'en': 'https://dl.dropboxusercontent.com/s/cz6koxli82rqdia/en.tar.gz?dl=0',
+    'pl': 'https://dl.dropboxusercontent.com/s/3nfolvuz7clzuz4/pl.tar.gz?dl=0',
+    'ru': 'https://dl.dropboxusercontent.com/s/6tzfxy34xx34mm7/ru.tar.gz?dl=0',
+    'uk': 'https://dl.dropboxusercontent.com/s/b76p4sc1lld96lw/uk.tar.gz?dl=0',
+    'tr': 'https://dl.dropboxusercontent.com/s/1wy01nq5fpq8iay/tr.tar.gz?dl=0',
+    'es': 'https://dl.dropboxusercontent.com/s/k6g5vj3x0rx7mjz/es.tar.gz?dl=0',
+    'cs': 'https://dl.dropboxusercontent.com/s/369wplqb0w2ax21/cs.tar.gz?dl=0',
 }
